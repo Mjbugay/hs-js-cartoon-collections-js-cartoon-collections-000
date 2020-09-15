c143be4625ff4c8fcb28813b9401651555b1d533
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-09-15 18:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var names = "";
-  for (var i = 0; i < dwarves.length / 2; i++) {
+  for (var i = 0; i < dwarves.length; i++) {
     names += `${(i+1)}. ${dwarves[i]} `;
   }
   return names;
@@ -41,12 +41,3 @@ function findTheCheese (foods) {
       return "no cheese!";
 }
 
-function wordsWithB (words) {
-  var results = [];
-  for (var i = 0; i < words.length; i++) {
-    if (word[i][0] == "b") {
-      results.push(word[i]);
-    }
-  }
-  return results;
-}
